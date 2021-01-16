070248ef0f93c3890956c2ffd44b841ecc581d7a
kiriake <kiriake@cloudpack.jp>
2021-01-16 21:19:55 +0900

    iroiro

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt -y upgrade
+if [ "$(uname)" == "Darwin" ]; then
+  OS="MacOS"
+  sudo easy_install pip
+  sudo pip install ansible
+  sudo pip install ansible --upgrade
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  OS="Linux"
+  sudo apt update
+  sudo apt -y upgrade
+  sudo apt install -y ansible
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then                                                                                           
+  OS='Cygwin'
+fi
 
-sudo apt install -y ansible
+if [ ! $OS ]; then
+  echo "Your platform is not supported. "
+  exit 1
+fi
+
 mkdir ~/git
 cd ~/git
 git clone https://github.com/kiriake/dotfiles.git
